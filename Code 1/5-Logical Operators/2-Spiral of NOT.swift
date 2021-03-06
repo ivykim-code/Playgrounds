b0f520326cc9bb67db49428ea80bdb7ea0417bf9
ivykim-code <77889925+ivykim-code@users.noreply.github.com>
2021-03-06 11:06:06 -0500

    Update 2-Spiral of NOT.swift

--- Code 1/5-Logical Operators/2-Spiral of NOT.swift
+++ Code 1/5-Logical Operators/2-Spiral of NOT.swift
@@ -1,1 +1,11 @@
-//
+//I did not finish
+for i in 1 ... 13 {
+    if !isBlocked {
+        moveForward()
+    }else{
+        turnLeft()
+    }else if isOneClosedSwitch{
+        toggleSwitch()
+    }
+    
+}
