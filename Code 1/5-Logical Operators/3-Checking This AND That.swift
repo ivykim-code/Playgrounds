2426332f9e4daaf3738ecf1991c34b2d6a7c32ab
ivykim-code <77889925+ivykim-code@users.noreply.github.com>
2021-03-27 11:26:21 -0400

    Update 3-Checking This AND That.swift

--- Code 1/5-Logical Operators/3-Checking This AND That.swift
+++ Code 1/5-Logical Operators/3-Checking This AND That.swift
@@ -1,1 +1,25 @@
 //
+
+func comingBack() {
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    turnRight()
+}
+
+for i in 1 ... 7 {
+    moveForward()
+    if !isOnGem{
+        
+    }else if isOnGem && isBlockedLeft{
+        collectGem()
+        turnRight()
+        moveForward()
+        moveForward()
+        toggleSwitch()
+        comingBack()
+    }else{
+        collectGem()
+    }
+}
