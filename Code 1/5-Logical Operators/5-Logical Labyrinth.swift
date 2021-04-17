06bb57fa6ae8fadbf997bcd7129d61a3fe3d6e0c
ivykim-code <77889925+ivykim-code@users.noreply.github.com>
2021-04-17 10:06:30 -0400

    Update 5-Logical Labyrinth.swift
    DONE!!!!!

--- Code 1/5-Logical Operators/5-Logical Labyrinth.swift
+++ Code 1/5-Logical Operators/5-Logical Labyrinth.swift
@@ -1,1 +1,27 @@
 //
+func letsGoDownAndCollectGem() {
+    moveForward()
+    moveForward()
+    collectGem()
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    turnRight()
+}
+
+
+for i in 1 ... 8 {
+    moveForward()
+    if isOnClosedSwitch && isOnGem{
+        toggleSwitch()
+        collectGem()
+        turnRight()
+        letsGoDownAndCollectGem()
+    }else if isOnClosedSwitch {
+        toggleSwitch()
+        turnLeft()
+    }else if isOnGem{
+        collectGem()
+    }
+}
