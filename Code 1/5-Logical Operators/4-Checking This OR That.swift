d55a696fe394c9659e9d1e76604c8398c18048d8
ivykim-code <77889925+ivykim-code@users.noreply.github.com>
2021-04-17 09:34:46 -0400

    Update 4-Checking This OR That.swift
    Done!!!!!!!!

--- Code 1/5-Logical Operators/4-Checking This OR That.swift
+++ Code 1/5-Logical Operators/4-Checking This OR That.swift
@@ -1,29 +1,7 @@
-//
 for i in 1 ... 12 {
     moveForward()
-    if isBlocked {
+    if isBlocked || isBlockedLeft{
         turnRight()
     }
-    
 }
-
-
-
-/* 
- move up
- moveForward()
- if blocked turn right
- if isBlocked{
- turnRight()
- }
- continue moving until blocked on the left
- moveForward moveForward
- turn right and move forward
- if isblockedleft{
- turnRight()
- moveForward()
- }
- if blocked on the front or on the left then turn right and move forward.
- 
-
- */
+collectGem()
