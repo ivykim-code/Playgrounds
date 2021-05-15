8ea6f5cd778fa80ecd278bb38c31baa28092a21e
ivykim-code <77889925+ivykim-code@users.noreply.github.com>
2021-05-15 11:07:38 -0400

    Update 9-You r Always Right.swift
    DONE!!!!!!!!!!!!!!!!!

--- Code 1/6-While Loops/9-You r Always Right.swift
+++ Code 1/6-While Loops/9-You r Always Right.swift
@@ -1,1 +1,20 @@
 //
+while !isOnGem{
+        while !isBlocked {
+            moveForward()
+            if isOnClosedSwitch {
+                toggleSwitch()
+            }
+        };turnRight()
+        
+    };collectGem()
+
+
+/*
+ while is on close switch then toggle switch
+ else moveForward
+ continue until blocked
+ when blocked turn right
+ REPEAT AGAIN (2)
+ TURN RIGHT in every corner
+ */
